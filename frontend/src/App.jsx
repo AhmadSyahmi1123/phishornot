30069140282eff211ffb995a04a5c0d2f19e30d2
Ahmad Syahmi <ahmad.syahmi3211@gmail.com>
2025-06-08 22:58:13 +0800

    edit the response fetch link

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch('http://localhost:8000/predict', {
+      const response = await fetch('https://phishornot-api.onrender.com/predict', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
